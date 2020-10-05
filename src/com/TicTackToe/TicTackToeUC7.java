860e63db1c694598af9830d7069cdfb921f9cd22
keshavanandsingh <keshavanand22@gmail.com>
2020-10-06 01:59:40 +0530

    UC8 added

--- src/com/TicTackToe/TicTackToeUC7.java
+++ src/com/TicTackToe/TicTackToeUC7.java
@@ -26,7 +26,7 @@ public class TicTackToeUC7 {
 			comp1 = 'x';
 	}
 
-	//Showing the board for users to choose their position
+	// Showing the board for users to choose their position
 	private void showBoard() {
 		int temp = 0;
 		for (int i = 0; i < 3; i++) {
@@ -61,8 +61,18 @@ public class TicTackToeUC7 {
 		}
 	}
 
-	//To randomly decide who plays first
-	private void tossWinner() {
+	private void comp1Move() {
+		int computerMoveIndex = (int) (Math.random() * 10 % 9) + 1;
+		if (userInputMoveValidate(computerMoveIndex)) {
+			board[computerMoveIndex] = comp1;
+			System.out.println();
+			showBoard();
+		} else
+			comp1Move();
+	}
+
+	// To randomly decide who plays first
+	private int tossWinner() {
 		int randomToss = (int) (Math.random() * 10 % 2);
 		int heads = 0;
 		int tails = 1;
@@ -70,10 +80,11 @@ public class TicTackToeUC7 {
 			System.out.println("player1 plays first");
 		else
 			System.out.println("comp1 plays first");
+		return randomToss;
 	}
 
-	//Conditions to check the winnings cases
-	private boolean winningCases(){
+	// Conditions to check the winnings cases
+	private boolean winningCases() {
 		if ((board[1] == 'x' && board[2] == 'x' && board[3] == 'x')
 				|| (board[1] == 'o' && board[2] == 'o' && board[3] == 'o'))
 			return true;
@@ -108,8 +119,54 @@ public class TicTackToeUC7 {
 		char board[] = ticTackToe.createBoard();
 		ticTackToe.player1Letter();
 		ticTackToe.showBoard();
-		ticTackToe.makeMove();
-		ticTackToe.tossWinner();
+		int firstPlayer = ticTackToe.tossWinner();
+		if (firstPlayer == 1) {
+			int flag = -1;
+			for (int i = 1; i <= 9; i++) {
+				ticTackToe.makeMove();
+				boolean playerWins = ticTackToe.winningCases();
+				if (playerWins) {
+					flag = 1;
+					break;
+				}
+				ticTackToe.comp1Move();
+				boolean computerWins = ticTackToe.winningCases();
+				if (computerWins) {
+					flag = 0;
+					break;
+				}
 
+			}
+			if (flag == 1)
+				System.out.println("Player1 wins the game");
+			else if (flag == 1)
+				System.out.println("Comp1 wins the game");
+			else
+				System.out.println("Game draw");
+		} else {
+			int flag = -1;
+			for (int i = 1; i <= 9; i++) {
+				ticTackToe.comp1Move();
+				boolean computerWins = ticTackToe.winningCases();
+				if (computerWins) {
+					flag = 0;
+					break;
+				}
+				ticTackToe.makeMove();
+				boolean playerWins = ticTackToe.winningCases();
+				if (playerWins) {
+					flag = 1;
+					break;
+				}
+
+			}
+			if (flag == 1)
+				System.out.println("Player1 wins the game");
+			else if (flag == 1)
+				System.out.println("Comp1 wins the game");
+			else
+				System.out.println("Game draw");
+
+		}
 	}
 }
